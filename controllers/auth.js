b9323b4fd1f5ae483ad5d94eabc92b6165fefdae
Nazar-Kotiv <kotnazar20@gmail.com>
2024-05-08 22:49:10 +0200

    commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -60,7 +60,7 @@ export const logout = async (req, res, next) => {
   try {
     await User.findByIdAndUpdate(req.user.id, { token: null });
 
-    res.status(204).end();
+    res.status(204).send({ message: "Registration successful" });
   } catch (error) {
     next(error);
   }
